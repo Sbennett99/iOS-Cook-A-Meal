710ee9b9c9928c57d114ebc0c29995da7de13b54
Seth Bennett <seth81599@hotmail.com>
2022-04-04 14:02:01 -0500

    Initial Commit

---
+++ Cook_A_Meal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cook_A_Meal
+//
+//  Created by student on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
